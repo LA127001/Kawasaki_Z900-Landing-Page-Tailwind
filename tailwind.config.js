33e210f61e3a925c7118c2fbaafd5ac10c3f82d9
Lakshya Agrawal <lakshyaagrawal775@gmail.com>
2024-08-20 11:15:11 +0530

    Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#1d613c',
+        'main' : '#060606',
+        'shw' : '#113a24'
+      },
+      fontFamily:{
+        'cdisplay' : ['Inter','sans-serif'],
+        'cbody' : ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
